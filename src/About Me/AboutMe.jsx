838fac9ea05de06844dc6dcb8a6bbc1629f20664
Andri Rafti <andrirafti@Andris-MacBook-Air.local>
2021-05-09 11:59:09 -0400

    update

--- src/About Me/AboutMe.jsx
+++ src/About Me/AboutMe.jsx
@@ -15,11 +15,11 @@ export default function AboutMe() {
                   <Tilt className="Tilt" options={{max:25,speed:400,glare:false,easing:"cubic-bezier(.03,.98,.52,.99)"}}>
       <img className="imgAndri"src="AndriPic.png"/>
                     
-        <p>Tenacious,Ambitious,Over-Achieving is what you would often here my co-workers refer to me as.</p>
-        <p>Every past role I always strived to not only be the best version of myself, but to always push myself to hit new limits. </p>
-        <p>I got into Software Engineering the Summer of 2020.</p>
-        <p>Over the course of the last year i have taken Udemy Courses, Self studied and graduated from GA Assemblys Immersive Program!</p>
-        <p>There were many times i was challenged but i never let that deter to from always learning, the prospect of always being able to improve your skills in such a field truly excites me and I can not wait to see where my career takes me.</p>
+        <p>Tenacious,Ambitious,Over-Achieving is what you would often hear my co-workers refer to me as.</p>
+        <p>Every past role I always strived to not only be the best version of myself, but to also strive to learn new ways of completeing my objective more efficently and adequately. </p>
+        <p>I got into the Software Engineering field the Summer of 2020.</p>
+        <p>Over the course of the last year I have taken Udemy Courses, self studied and graduated from GA Assemblys Immersive BootCamp Program!</p>
+        <p>There were many times I was challenged,however I never let that deter me from always learning the next new thing. The prospect of always being able to improve your skills in such a field truly excites me and I can not wait to see where my career takes me.</p>
       </Tilt>
       </div>
   )
